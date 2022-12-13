534cea74185484e5f8f262c80fdbc22445516b65
George Aldington <georgealdington@gmail.com>
2022-12-13 14:27:45 +0000

    installed dotenv

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 
-const APP_PORT = 4000;
+const APP_PORT = process.env.PORT || 4000;
 
 app.listen(APP_PORT, () => {
   console.log(`My app is listening on port ${APP_PORT}`)
