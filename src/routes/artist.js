9997fb43a4a90b76d3316cbd9d5d074deab25605
George Aldington <georgealdington@gmail.com>
2022-12-16 15:37:50 +0000

    added a try catch to my route

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -2,9 +2,13 @@ const db = require('../db/index');
 
 const artistRoute = async (req, res) => {
     const { name, genre } = req.body
-  
+    console.log("hello");
+    try {
       const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
       res.status(201).json(artist)
+    } catch (err) {
+      res.status(500).json(err.message)
+    }
   };
 
 module.exports = artistRoute;
